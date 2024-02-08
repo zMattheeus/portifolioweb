e7f4680e6d0263a05db26d42b11f760e9f4e6d33
zMattheeus <matheusrodrigueslima33@gmail.com>
2024-02-08 11:58:56 -0300

    remocao dos modulos por erro no router link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './pages/home/home.module';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HamburgerComponent } from './shared/hamburger/hamburger.component';
+import { MainComponent } from './shared/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    HamburgerComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
