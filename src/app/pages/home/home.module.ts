cb7a527801801360538a1801fb52638208eafdef
zMattheeus <matheusrodrigueslima33@gmail.com>
2024-02-08 02:09:58 -0300

    main component finalizado

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { HamburgerComponent } from 'src/app/shared/hamburger/hamburger.component';
+import { MainComponent } from 'src/app/shared/main/main.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    HamburgerComponent
+    HamburgerComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
